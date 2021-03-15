71056af9fefcfaf6b15d9349108f2146432bc028
YohannBe <48290347+YohannBe@users.noreply.github.com>
2021-03-15 13:08:06 +0100

    TaskViewModelTest bug -> empty method doe not work yet, observerForever point on a null object reference

--- app/src/androidTest/java/com/cleanup/todoc/TaskViewModelTest.java
+++ app/src/androidTest/java/com/cleanup/todoc/TaskViewModelTest.java
@@ -1,10 +1,16 @@
 package com.cleanup.todoc;
 
 
+import android.content.Context;
+
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
+import com.cleanup.todoc.injection.Injection;
+import com.cleanup.todoc.injection.ViewModelFactory;
 import com.cleanup.todoc.model.Task;
 import com.cleanup.todoc.repositories.ProjectDataRepository;
 import com.cleanup.todoc.repositories.TaskDataRepository;
@@ -21,28 +27,32 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 
 @RunWith(JUnit4.class)
 
 public class TaskViewModelTest {
     @Rule
     public InstantTaskExecutorRule instantExecutor = new InstantTaskExecutorRule();
+
     private TaskViewModel viewModel;
     private TaskDataRepository taskDataRepository;
     private ProjectDataRepository projectDataRepository;
+    private Executor executor = Executors.newSingleThreadExecutor();
+    private Context context;
 
 
     @Before
     public void init(){
         taskDataRepository = mock(TaskDataRepository.class);
         projectDataRepository = mock(ProjectDataRepository.class);
-        viewModel = new TaskViewModel(projectDataRepository, taskDataRepository, (Executor) instantExecutor);
+        context = mock(Context.class);
+        this.viewModel = new TaskViewModel(projectDataRepository, taskDataRepository, executor);
     }
 
     @Test
     public void empty() {
-        Observer result = mock(Observer.class);
+        Observer <List<Task>> result = mock(Observer.class);
         viewModel.getAllTasks().observeForever(result);
-
     }
 }
